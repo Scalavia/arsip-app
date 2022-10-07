2be2da6936156bffeed1662ba4bfa2ab4dde8b9e
Scalavia <ihzamahendra4814@gmail.com>
2022-10-07 13:27:18 +0700

    add table database

--- app/Models/Arsip.php
+++ app/Models/Arsip.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Arsip extends Model
 {
     use HasFactory;
+
+    protected $table = 'arsips';
+    protected $fillable = ['nomor_surat', 'kategori', 'judul', 'nama_file'];
+    protected $guard = [];
 }
